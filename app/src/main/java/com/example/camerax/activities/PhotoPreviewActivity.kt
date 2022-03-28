0bb6c394c8f12c187797ac3d74c95d7ebbe71f9d
Shijo C S <shijocs07@gmail.com>
2022-03-28 12:04:24 +0530

    added missed commit. added image preview in PhotoPreviewActivity.kt

--- app/src/main/java/com/example/camerax/activities/PhotoPreviewActivity.kt
+++ app/src/main/java/com/example/camerax/activities/PhotoPreviewActivity.kt
@@ -2,11 +2,27 @@ package com.example.camerax.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.example.camerax.R
+import java.io.File
 
 class PhotoPreviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_preview)
+
+        try {
+            val file = File(intent.getStringExtra("file"))
+            if(file.exists()) {
+                Glide
+                    .with(this)
+                    .load(file)
+                    .centerCrop()
+                    .placeholder(android.R.drawable.progress_horizontal)
+                    .into(findViewById(R.id.image_view))
+            }
+        } catch (e : Exception) {
+
+        }
     }
 }
